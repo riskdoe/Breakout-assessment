c98acd5f5bf2bcfbc59530e24e79ea147add02d6
PoiDoe <tarryntw@gmail.com>
2021-09-22 16:19:51 +1200

    Update Paddle.java

--- Assignment 3/Paddle.java
+++ Assignment 3/Paddle.java
@@ -6,18 +6,29 @@ public class Paddle extends Sprite {
 	
 	public Paddle() {
 		// TODO: Set width to Settings.PADDLE_WIDTH
+		width = Settings.PADDLE_WIDTH;
 		// TODO: Set width to Settings.PADDLE_HEIGHT
+		height = Settings.PADDLE_HEIGHT;
 		// TODO: Call resetPosition
+		resetPosition();
 	}
 	
 	public void resetPosition() {
 		// TODO: Set initial position x and y (use INITIAL_PADDLE_X/Y)
+		setX(Settings.INITIAL_PADDLE_X);
+		setY(Settings.INITIAL_PADDLE_Y);
 		// Note: Check Ball.java for a hint
 	}
 	
 	public void update() {
 		x += xVelocity;
 		
+		if (x <= 0) {
+			x = 0;
+		}
+		else if (x + width >= Settings.WINDOW_WIDTH) {
+			x = Settings.WINDOW_WIDTH - width;
+		}
 		// TODO: Prevent the paddle from moving outside of the screen
 		// This can be done using two if statements (one for the left side of the screen and one for the right)
 
