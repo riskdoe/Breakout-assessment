aeb61a914fb98e6d81d4a623c14cbc81705c73d9
PoiDoe <tarryntw@gmail.com>
2021-09-22 16:34:03 +1200

    Update Brick.java

--- Assignment 3/Brick.java
+++ Assignment 3/Brick.java
@@ -5,16 +5,20 @@ public class Brick extends Sprite {
 	private boolean broken = false;
 	
 	public Brick(int x, int y) {
-		// TODO: Set x using the parameter
+		// TODO: Set x using the parameter4
+		setX(x);
 		// TODO: Set y using the parameter
+		setY(y);
 		// TODO: Set the width and height of the brick using Settings.BRICK_WIDTH/HEIGHT
+		setWidth(Settings.BRICK_WIDTH);
+		setHeight(Settings.BRICK_HEIGHT);
 	}
 
 	public boolean isBroken() {
-		return false;	// TODO: Return the correct variable
+		return broken;	// TODO: Return the correct variable
 	}
 	public void setBroken(boolean b) {
-		// TODO: Set the broken variable using the parameter given
+		broken = b;
 	}
 	
 	public void paint(Graphics g) {
